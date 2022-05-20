8a15f705682fbbd8f9c447c3c1e388696da54744
Muzahidul Islam <muzahidul190@gmail.com>
2022-05-21 03:09:20 +0600

    procfile added

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Logged in as ${client.user.tag}!`);
-        client.user.setActivity("Muza's PC! 😐", { type: 'COMPETING' });
+        client.user.setActivity("on the web 🕸️", { type: 'COMPETING' });
         client.user.setStatus('idle');
         // console.log(client.commands);
         console.log("PCN is up and ready!");
