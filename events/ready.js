d1ce21c7a0e39ca31708ba0e13af08acb35903bc
Muzahidul Islam <muzahidul190@gmail.com>
2022-05-23 13:05:16 +0600

    Competing in the web

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Logged in as ${client.user.tag}!`);
-        client.user.setActivity("on the web 🕸️", { type: 'COMPETING' });
+        client.user.setActivity("the web 🕸️", { type: 'COMPETING' });
         client.user.setStatus('idle');
         // console.log(client.commands);
         console.log("PCN is up and ready!");
