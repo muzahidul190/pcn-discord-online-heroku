b96d3bf9c0be6d5bfd5ee45e242bd0270b4894f6
Muzahidul Islam <muzahidul190@gmail.com>
2022-05-23 11:06:30 +0600

    find by id

--- events/guidMemberAdd.js
+++ events/guidMemberAdd.js
@@ -1,12 +1,9 @@
 const { roleManager, constructImageAndSend } = require('../helperFunctions');
 
-
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
-        const guild = member.guilds.cache.get(process.env.guildId);
-        const channelId = "965667610903597139";
-        const channel = guild.channels.cache.get(channelId);
+        const channel = member.guild.channels.cache.find(channel => channel.id === '965667610903597139');
         if (!channel) {
             console.log('Channel not found.');
             return;
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,9 +3,7 @@ const { constructImageAndSend } = require('../helperFunctions');
 module.exports = {
     name: 'guildMemberRemove',
     async execute(member) {
-        const guild = member.guilds.cache.get(process.env.guildId);
-        const channelId = "965667610903597139";
-        const channel = guild.channels.cache.get(channelId);
+        const channel = member.guild.channels.cache.find(channel => channel.id === '965667610903597139');
         if (!channel) {
             console.log('Channel not found.');
             return;
