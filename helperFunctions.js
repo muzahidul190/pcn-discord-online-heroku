27dfedab7d1df24a8c098d43998902f243b4d250
Muzahidul Islam <muzahidul190@gmail.com>
2022-05-30 17:48:04 +0600

    commented

--- helperFunctions.js
+++ helperFunctions.js
@@ -16,9 +16,9 @@ function deleteMessage(msgObj) {
 
 function messageHandler(message) {
     if(message.content.toLowerCase() === 'iama' && message.channel.id === "980787684391915520"){
-        const user = message.guild.members.fetch(message.author.id);
-        sendMessage(message, `Okay, you are added.`);
-        roleManager(user, "974722363386122280", true, true);
+        // const user = message.guild.members.fetch(message.author.id);
+        // sendMessage(message, `Okay, you are added.`);
+        // roleManager(user, "974722363386122280", true, true);
     }else if (message.content.toLowerCase() === 'hello') {
         sendMessage(message, `Hi @${message.author.username}`);
     }else if(message.content.toLowerCase() === 'invite'){
