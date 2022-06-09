31616541fd332d81d19f902b09b465d637b051c0
Muzahidul Islam <muzahidul190@gmail.com>
2022-06-10 00:08:58 +0600

    {} regx added

--- helperFunctions.js
+++ helperFunctions.js
@@ -33,7 +33,12 @@ function messageHandler(message) {
             sendMessage(message, `Oops!!, There is a syntax error in your code. I doubt if you are a true contestant or not 😕`);
         }
         else{
-            sendMessage(message, `⛔ Please don't send random text here.`)
+            const regex = /\{|\}/g
+            if (message.content.match(regex)) {
+                sendMessage(message, 'Please read this message more carefully to become a contestant! \nMessage Link: https://discord.com/channels/965667610903597136/974719552929796108/980830360633999410 ')
+                return;
+            }
+            else sendMessage(message, `⛔ Please don't send random text here.`);
         }
     }else if (message.content.toLowerCase() === 'hello') {
         sendMessage(message, `Hi @${message.author.username}`);
